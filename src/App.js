72e73b6b2cfa0502137f73e00e4c12b91d450f2f
Dmitry Tartsaiev <dmitantar80@gmail.com>
2021-12-12 21:21:41 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { nanoid } from 'nanoid'
+import './App.css'
+import Container from './components/Container/Container'
+import Phonebook from './components/Phonebook/Phonebook'
+import Contacts from './components/Contacts/Contacts'
+import Filter from './components/Filter/Filter'
+import { getFromLS, setInLS } from './utilits/localstorage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  componentDidMount() {
+    const contacts = getFromLS('contacts')
+    if (contacts) {
+      this.setState({ contacts })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state
+    if (contacts !== prevState.contacts) {
+      setInLS('contacts', contacts)
+    }
+  }
+
+  onAddContact = (name, number) => {
+    if (this.onCheckContact(name)) {
+      alert(`${name} is already in contacts`)
+      return
+    }
+    const obj = { id: nanoid(), name, number }
+    this.setState((prevState) => ({ contacts: [...prevState.contacts, obj] }))
+  }
+
+  onCheckContact = (value) => {
+    return this.state.contacts.find(
+      (el) => el.name.toUpperCase() === value.toUpperCase(),
+    )
+  }
+
+  onDeleteContacts = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((el, index) => el.id !== id),
+    }))
+  }
+
+  onFiltering = (value) => {
+    this.setState({ filter: value })
+  }
+
+  render() {
+    const { contacts, filter } = this.state
+    return (
+      <div className="App">
+        <Container title="Phonebook">
+          <Phonebook onAddContact={this.onAddContact} />
+        </Container>
+        <Container title="Contacts">
+          {contacts.length >= 2 && (
+            <Filter filter={filter} onFilter={this.onFiltering} />
+          )}
+          <Contacts
+            listContacts={contacts.filter((el) =>
+              el.name.toUpperCase().includes(filter.toUpperCase()),
+            )}
+            onDelete={this.onDeleteContacts}
+          />
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
